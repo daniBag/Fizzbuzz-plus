7f46522f0a2a59637b1b12f8a95252947c6cc54d
daniel bagno <danielbagno@edu.aac.ac.il>
2025-08-07 23:05:14 +0300

    prime-check fix

--- fizzbuzz_plus.py
+++ fizzbuzz_plus.py
@@ -3,20 +3,17 @@ import math
 import argparse
 
 
-def is_primary(num):
-    prime = False
-    if num == 2 or num == 3:
-        prime = True
-    elif num > 1:
-        if num % 2 != 0:
-            if num % 3 != 0:
-                i = 5
-                while i <= math.sqrt(num):
-                    if (num % i == 0) or (num % (i + 2) == 0):
-                        return False
-                    i += 6
-                prime = True
-    return prime
+def is_prime(num: int) -> bool:
+    if num < 2: return False
+    if num == 2 or num == 3: return True
+    elif num % 2 == 0 or num % 3 == 0: return False
+    else:
+        i, limit = 5, math.isqrt(num)
+        while i <= limit:
+            if (num % i == 0) or (num % (i + 2) == 0):
+                return False
+            i += 6
+    return True
 
 
 def fizzbuzz(start=1, end=100):
@@ -27,29 +24,22 @@ def fizzbuzz(start=1, end=100):
         end = temp
     for i in range(start, end + 1):
         if i % 15 == 0:
-            result.append("fizzbuzz")
-        elif is_primary(i):
-            result.append("prime")
-        elif i % 3 == 0 and i != 3:
-            result.append("fizz")
+            result.append("Fizzbuzz")
+        elif is_prime(i):
+            result.append("Prime")
+        elif i % 3 == 0:
+            result.append("Fizz")
         elif i % 5 == 0:
-            result.append("buzz")
+            result.append("Buzz")
         else:
             result.append(str(i))
     return result
 
 if __name__ == "__main__":
     parser =argparse.ArgumentParser(description= "a simple script demonstrating loops, conditions and prime check")
-    parser.add_argument("start", help= "the starting number to iterate from")
-    parser.add_argument("end", help= "the last number to iterate to")
+    parser.add_argument("start", nargs="?", type=int, default=1, help= "the starting number to iterate from")
+    parser.add_argument("end", nargs="?", type=int, default=100, help= "the last number to iterate to")
     parser.add_argument("--json", action="store_true", help= "get the output as a JSON array")
     args = parser.parse_args()
-    try:
-        start = int(args.start)
-        end = int(args.end)
-        result = fizzbuzz(start, end)
-        if args.json:
-            result = json.dumps(result)
-        print(result)
-    except ValueError:
-        print("only numbers are accepted")
+    result = fizzbuzz(args.start, args.end)
+    print("\n".join(result) if not args.json else json.dumps(result))
--- test_fizzbuzz_plus.py
+++ test_fizzbuzz_plus.py
@@ -1,13 +1,21 @@
-from unittest.mock import patch
-
 from fizzbuzz_plus import fizzbuzz
-from io import StringIO
 import unittest
 
-correct_result = ["1", "prime", "prime", "4", "prime", "fizz", "prime", "8", "fizz", "buzz", "prime", "fizz", "prime", "14", "fizzbuzz", "16", "prime", "fizz", "prime", "buzz"]
+correct_result = ["1", "Prime", "Prime", "4", "Prime", "Fizz", "Prime", "8", "Fizz", "Buzz", "Prime", "Fizz", "Prime", "14", "Fizzbuzz", "16", "Prime", "Fizz", "Prime", "Buzz"]
 
 class TestNumsDivision(unittest.TestCase):
 
     def test_prints(self):
         result = fizzbuzz(1, 20)
         self.assertEqual(result, correct_result)
+
+    def test_reversed_range(self):
+        assert correct_result == fizzbuzz(20, 1)
+
+    def test_edge_cases(self):
+        assert fizzbuzz(25, 25) == ["25"]
+        assert fizzbuzz(49, 49) == ["49"]
+
+    def test_single_number_cases(self):
+        assert fizzbuzz(3,3) == ["Prime"]
+        assert fizzbuzz(15,15) == ["Fizzbuzz"]
